26f0dd463f583f52b6128628de2807c26fb1e5a3
EntropyGamesStudio <EntropyGamesStudio@gmail.com>
2015-12-09 11:38:47 +0000

    initial commit

---
+++ src/com/EntropyGamesStudio/rain/Game.java
@@ -0,0 +1,14 @@
+package com.EntropyGamesStudio.rain;
+
+public class Game {
+	
+	public static int width = 300;
+	public static int height = width / 16 * 9;
+	public static int scale = 3;
+	
+	private Thread thread;
+	
+	
+	
+
+}
