1767b89fe5db4d2c3a2600f840d3ecba98ab2997
EntropyGamesStudio <EntropyGamesStudio@gmail.com>
2015-12-09 12:42:27 +0000

    Added Window and Main method.

--- src/com/EntropyGamesStudio/rain/Game.java
+++ src/com/EntropyGamesStudio/rain/Game.java
@@ -1,13 +1,28 @@
 package com.EntropyGamesStudio.rain;
 
-public class Game implements Runnable{
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Frame;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable{ 
+	private static final long serialVersionUID = 1L;
 	
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
 	
 	private Thread thread;
+	private JFrame frame;
 	private boolean running = false;
+	
+	public Game(){
+		Dimension size = new Dimension(width * scale, height * scale);
+		setPreferredSize(size);
+		
+		frame = new JFrame(); 
+	}
 	
 	public synchronized void start() {
 		running = true;
@@ -27,10 +42,20 @@ public class Game implements Runnable{
 	@Override
 	public void run() {
 		while (running == true){
-			
+			System.out.println("Running...");
 		}	
 	}
 	
-
-	
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle("Rain");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null); //puts window in the centre of the screen.
+		game.frame.setVisible(true);
+		
+		game.start();
+	}
 }
